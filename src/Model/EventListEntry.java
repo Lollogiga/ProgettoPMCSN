d257acf74580a6d6ab3612fcbe3634026f44d1df
lollogiga <lorenzo.grande@hotmail.it>
2024-08-23 18:09:57 +0200

    Init1

---
+++ src/Model/EventListEntry.java
@@ -0,0 +1,10 @@
+package Model;
+
+import java.util.List;
+
+public class EventListEntry {
+    private int t;
+    public static int getNextEvent(List<EventListEntry> event){
+        return 0;
+    }
+}
